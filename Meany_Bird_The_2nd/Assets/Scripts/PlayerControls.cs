30ffa9d48f15fb46065bba81885f80410bde1d61
Walegi4 <116742021+yehans41@users.noreply.github.com>
2023-05-02 18:41:45 -0500

    jh
    j

--- Meany_Bird_The_2nd/Assets/Scripts/PlayerControls.cs
+++ Meany_Bird_The_2nd/Assets/Scripts/PlayerControls.cs
@@ -4,41 +4,36 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    //Game manager object
+    [Header("Game Controller Object for controlling the game")]
+    public GameController gameController;
+    [Header("Defult Velocity")]
+    public float velocity = 5;
+    //Physics for the bird
+    private Rigidbody2D rb;
+    //hight of the bird object on the y axis
+    private float objectHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //Game Controller componet
+        gameController = GetController<GameController>();
+        //Speed the game is at a playing state
+        Time.timeScale = 1;
+        rb = GetComponet<Rigidbody2D>();
+        // Object Height equals the size of the height of the sprite
+        objectHeight = transform.GetComponet<SpriteRenederer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the left mouse button is clicked
+        if (Input.GetMouseButtonDown(0))
+            //The bird will float up on the Y axis
+            //and float back down on the Y axis
+            rb.velocity = Vector2.up * velocity;
 
-    }
-}
-//Game manager object
-[Header("Game Controller Object for controlling the game")]
-public GameController gameController;
-[Header(""Defult Velocity")]
-public float velocity = 5;
-//Physics for the bird
-private Rigidbody2D rb;
-//hight of the bird object on the y axis
-private float objectHeight;
-void Start()
-{
-    //Game Controller componet
-    gameController = GetController<GameController>();
-    //Speed the game is at a playing state
-    Time.timeScale = 1;
-    rb = GetComponet<Rigidbody2D>();
-    // Object Height equals the size of the height of the sprite
-    objectHeight = transform.GetComponet<SpriteRenederer>().bounds.size.y / 2;
-//If the left mouse button is clicked
-if (Input.GetMouseButtonDown(0))
-    {
-        //The bird will float up on the Y axis
-        //and float back down on the Y axis
-        rb.velocity = Vector2.up * velocity;
     }
 }
